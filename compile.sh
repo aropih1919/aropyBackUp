71e2b835258f3b600bf09596681cea55d2a0eb40
aropih1919 <mpiarorakotozafy@gmail.com>
2026-06-15 18:48:33 +0300

    effacee

--- compile.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#Compilation
-mkdir -p build
-find . -name "*.java" > sources.txt
-javac -cp "lib/*" -d build @sources.txt
-rm sources.txt
-#Creation jar
-jar cf aropy-framework.jar -C build .
-sudo cp aropy-framework.jar /usr/share/tomcat10/lib
